57e1c3034026377521057577de1b80b5c969468a
Ali Tlisov <ali.tlisov@gmail.com>
2017-09-26 11:13:22 +0300

    added doctests. fixed markov module

--- src/Dynamic.hs
+++ src/Dynamic.hs
@@ -1,6 +1,17 @@
 module Dynamic where
-
+    
 import Data.MemoTrie (memo3)
+
+-- $setup
+-- >>>:set -XGeneralizedNewtypeDeriving
+-- >>> import Test.QuickCheck
+-- >>> import Control.Monad
+-- >>> import Control.Arrow
+--
+-- >>> newtype TInt = TInt {getInt :: Int} deriving (Eq, Ord, Show, Num, Integral, Real, Enum)
+--
+-- >>> instance Arbitrary TInt where arbitrary = liftM TInt (choose (1, 30) :: Gen Int) 
+--
 
 type Pos = (Int, Int)   -- ^ position is a pir of Int's
 
@@ -13,6 +24,8 @@ dynamic (x0, y0) (x, y) = dynamic0 (x - x0) (y - y0)
 
 -- | gives stabilized (Q, V) pair
 --
+-- This property says that stable V == min stable Q
+-- prop> True == (uncurry (==) $ minimum *** id $ dynamic0 (getInt x) (getInt y))
 dynamic0 :: Int              -- ^ x coordinate
          -> Int              -- ^ y coordinate
          -> ([Int], Int)     -- ^ return pair (Q, V)
@@ -27,17 +40,21 @@ qv :: Int -> Int -> Int -> ([Int], Int)
 qv x y n = (fqmem x y n, fvmem n x y)
 
 -- | memoized version of fq
---   
+-- 
+-- prop> fqmem (getInt x) (getInt y) (getInt n) == fq (getInt x) (getInt y) (getInt n)
 fqmem :: Int -> Int -> Int -> [Int]
 fqmem = memo3 fq
 
 -- | memoized version of fv
 --
+-- prop> fvmem (getInt n) (getInt x) (getInt y) == fv (getInt n) (getInt x) (getInt y)
 fvmem :: Int -> Int -> Int -> Int
 fvmem = memo3 fv
 
 -- | calculates Q 
 --
+-- prop> fq x y 0 == [0,0,0,0]
+-- prop> (minimum $ fq (getInt x) (getInt y) (getInt n)) >= fv (getInt n) (getInt x) (getInt y)
 fq  :: Int      -- ^ x coordinate 
     -> Int      -- ^ y coordinate
     -> Int      -- ^ step number
@@ -47,7 +64,16 @@ fq x y n = (cost (x, y) +) . (uncurry $ fvmem n) <$> neighbours (x, y)
 
 -- | calculates V
 --
--- prop> fv x 0 0 == 0
+-- prop> fv n 0 0 == 0
+-- prop> fv 0 x y == cost (x, y)
+--
+-- V is symmetric to substituting x and y
+-- prop> fv (getInt n) (getInt x) (getInt y) == fv (getInt n) (getInt y) (getInt x)
+--
+-- V is symmetric to changing sign of x and/or y
+-- prop> fv (getInt n) (getInt x) (getInt y) == fv (getInt n) (getInt (-x)) (getInt y)
+-- prop> fv (getInt n) (getInt x) (getInt y) == fv (getInt n) (getInt x) (getInt (-y))
+-- prop> fv (getInt n) (getInt x) (getInt y) == fv (getInt n) (getInt (-x)) (getInt (-y))
 fv  :: Int  -- ^ step number 
     -> Int  -- ^ x coordinate 
     -> Int  -- ^ y coordinate
--- src/Markov.hs
+++ src/Markov.hs
@@ -2,6 +2,16 @@ module Markov where
 
 import Dynamic
 
+-- $setup
+-- >>>:set -XGeneralizedNewtypeDeriving
+-- >>> import Test.QuickCheck
+-- >>> import Control.Monad
+--
+-- >>> newtype TInt = TInt {getInt :: Int} deriving (Eq, Ord, Show, Num, Integral, Real, Enum)
+--
+-- >>> instance Arbitrary TInt where arbitrary = liftM TInt (choose (1, 30) :: Gen Int) 
+--
+
 -- | combines many targets with their probabilities to give 4 cost values in current position
 --
 markovOut   :: Pos      -- ^ initial position
@@ -15,30 +25,38 @@ markovOut x0 xs ps = foldl (zipWith (+)) [0,0,0,0] pcosts
             pcosts = let f p cs = (p *) . fromIntegral <$> cs 
                      in zipWith f ps costs
 
--- | 'bayes_a' apriory probability of command for particular target
--- P(a | x, y, n) = exp (V(x, y, n) - Q(x, y, a, n))
+-- | 'bayesPrior' apriory probability of command for particular target
+-- P (a | x, y, n) = exp (V (x, y, n) - Q (x, y, a, n))
 -- as V == min Q this will be: exp (-inf, 0] -> (0, 1]
 -- I believe this 'exp' is just a dirty hack. It just produces plausible values
-bayes_a :: Int      -- ^ command
-        -> Pos      -- ^ current position
-        -> Pos      -- ^ target position
-        -> Double   -- ^ apriory probability of user choosing command in current situation
-bayes_a a p0 p = exp . fromIntegral $ v - q !! a
+--
+-- Probability is less than 1
+-- prop> bayesPrior 3 (getInt x, getInt y) (getInt x1, getInt y1) <= 1.0
+bayesPrior  :: Int      -- ^ command
+            -> Pos      -- ^ current position
+            -> Pos      -- ^ target position
+            -> Double   -- ^ apriory probability of user choosing command in current situation
+bayesPrior a p0 p = exp . fromIntegral $ v - (q !! a)
     where (q, v) = dynamic p0 p
 
 
--- | 'bayes_p' posteriory probability of particular target given command a
--- p(a | x, y, n) = P(a | x, y, n) / Sum (P(a | x, y, ...))
-bayes_p :: Int      -- ^ command
-        -> Pos      -- ^ current position
-        -> [Pos]    -- ^ target positions
-        -> Int      -- ^ target index
-        -> Double   -- ^ posteriory probzbility after user issued some command
-bayes_p a p0 ps n = let prob = bayes_a a p0
+-- | 'bayesPosterior' posteriory probability of particular target given command a
+-- p (a | x, y, n) = P (a | x, y, n) / Sum (P (a | x, y, ...))
+--
+-- Probability is less than 1
+-- prop> bayesPosterior 3 (getInt x, getInt y) [(getInt x1, getInt y1),(getInt x2, getInt y2)] 1 <= 1.0
+bayesPosterior  :: Int      -- ^ command
+                -> Pos      -- ^ current position
+                -> [Pos]    -- ^ target positions
+                -> Int      -- ^ target index
+                -> Double   -- ^ posteriory probzbility after user issued some command
+bayesPosterior a p0 ps n = let prob = bayesPrior a p0
                     in prob (ps !! n) / sum (prob <$> ps)
 
 -- | This function recalculates probability of target when command from user arrives
 --
+-- Probability is less than 1
+-- prop> markovInEach (getInt x, getInt y) [(x1,y1),(x2,y2)] [0.75,0.25] 0 0 <= 1.0
 markovInEach    :: Pos      -- ^ current position
                 -> [Pos]    -- ^ positons of targets
                 -> [Double] -- ^ probabilities of targets before user command
@@ -46,15 +64,17 @@ markovInEach    :: Pos      -- ^ current position
                 -> Int      -- ^ command issued by user
                 -> Double   -- ^ new probability of target
 markovInEach p ps pr n a =  bayes n * (pr !! n) / sum (zipWith (*) bayeses pr)
-                    where bayes = bayes_p a p ps 
-                          bayeses = fmap bayes [1 .. length pr]
+                    where bayes = bayesPosterior a p ps 
+                          bayeses = fmap bayes [0 .. length pr - 1]
 
 -- | Updates all probabilities on user's input
 -- 
+-- All probabilities should sum to 1
+-- prop> abs (sum (markovIn (getInt x, getInt y) [(x1,y1),(x2,y2)] [0.75,0.25] 1) - 1.0) < 1e-6
 markovIn    :: Pos      -- ^ current position
             -> [Pos]    -- ^ positons of targets
             -> [Double] -- ^ probabilities of targets before user command
             -> Int      -- ^ command issued by user
             -> [Double] -- ^ new probability of target
-markovIn p ps pr a = markovInEach' <$> [1 .. length pr]
+markovIn p ps pr a = markovInEach' <$> [0 .. length pr - 1]
     where markovInEach' n = markovInEach p ps pr n a
