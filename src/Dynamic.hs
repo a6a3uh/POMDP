cd86200b43cee30479a16fb8a402d179e385f81b
Ali Tlisov <ali.tlisov@gmail.com>
2017-10-24 19:28:50 +0300

    compiles

--- src/Dynamic.hs
+++ src/Dynamic.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE  FlexibleContexts
+            , ConstraintKinds #-}
+
 module Dynamic where
     
-import Data.MemoTrie (memo3)
+-- import Data.MemoTrie (memo3)
 import Control.Monad.Reader
+import Control.Monad.Memo
 
 -- $setup
 -- >>>:set -XScopedTypeVariables
@@ -11,12 +15,15 @@ import Control.Monad.Reader
 -- >>> import Data.List
 --
 -- >>> newtype Bounded = Bounded Int deriving Show
--- >>> instance Arbitrary Bounded where arbitrary = Bounded . (`mod` 4) <$> arbitrary
+-- >>> instance Arbitrary Bounded where arbitrary = Bounded . (`mod` 3) <$> arbitrary
 -- >>> newtype BoundedPositive = BoundedPositive Int deriving Show
--- >>> instance Arbitrary BoundedPositive where arbitrary = BoundedPositive  . succ . (`mod` 4) . abs <$> arbitrary
+-- >>> instance Arbitrary BoundedPositive where arbitrary = BoundedPositive  . succ . (`mod` 2) . abs <$> arbitrary
 
 type Pos = (Int, Int)   -- ^ position is a pir of Int's
-type Cost = Pos -> Double
+-- type Cost n r = (Integral n, Integral n, Floating r) -> (n, n) -> r
+
+type Dynamic n r m = (Integral n, Floating r, Ord r, MonadReader ((n, n) -> r) m, MonadMemo (n, n, n) [r] m)
+-- type MemoQ = MemoT (Int, Int, Int) [Double]
 
 -- | gives (Q, V) for moving from curretn point to arbitraty point
 --
@@ -35,9 +42,10 @@ type Cost = Pos -> Double
 -- True
 -- >>> uncurry (>) $ (!!0) &&& (!!1) $ fst (runReader (dynamic (0, 0) (3, 3)) cost)
 -- True
-dynamic :: Pos          -- ^ current position
-        -> Pos          -- ^ target position
-        -> Reader Cost ([Double], Double) -- ^ pair (Q V)
+dynamic :: Dynamic n r m--(Integral n, Real r, MonadReader Cost m, MonadMemo (n, n, n) [r] m)
+        => (n, n)--Pos          -- ^ current position
+        -> (n, n)--Pos          -- ^ target position
+        -> m ([r], r) -- ^ pair (Q V)
 dynamic (x0, y0) (x, y) = dynamic0 (x0 - x) (y0 - y)
 
 -- | gives stabilized (Q, V) pair
@@ -52,9 +60,10 @@ dynamic (x0, y0) (x, y) = dynamic0 (x0 - x) (y0 - y)
 -- In the setup below moving down is a best choise
 -- >>> let costs = fst (runReader (dynamic0 0 3) cost) in elemIndex (minimum costs) costs
 -- Just 2
-dynamic0 :: Int              -- ^ x coordinate
-         -> Int              -- ^ y coordinate
-         -> Reader Cost ([Double], Double)     -- ^ return pair (Q, V)
+dynamic0 :: Dynamic n r m --(Integral n, Real r, MonadReader Cost m, MonadMemo (n, n, n) [r] m)
+         => n              -- ^ x coordinate
+         -> n              -- ^ y coordinate
+         -> m ([r], r)     -- ^ return pair (Q, V)
 dynamic0 x y = do
     qv <- traverse (qv x y) [1 ..]
     return $ lastUnique qv
@@ -64,23 +73,27 @@ dynamic0 x y = do
 
 -- | gives pair of memoized Q and V
 --
-qv :: Int -> Int -> Int -> Reader Cost ([Double], Double)
+qv  :: Dynamic n r m --(Integral n, Real r, MonadReader Cost m, MonadMemo (n, n, n) [r] m)
+    => n 
+    -> n 
+    -> n 
+    -> m ([r], r)
 qv x y n = do
-    q <- fqmem x y n
-    v <- fvmem n x y
+    q <- fq x y n
+    v <- fv n x y
     return (q, v)
 
--- | memoized version of fq
--- 
--- prop> \(NonNegative n) (Bounded x) (Bounded y) -> runReader (fqmem x y n) cost == runReader (fq x y n) cost
-fqmem :: Int -> Int -> Int -> Reader Cost [Double]
-fqmem = memo3 fq
+-- -- | memoized version of fq
+-- -- 
+-- -- prop> \(NonNegative n) (Bounded x) (Bounded y) -> runReader (fqmem x y n) cost == runReader (fq x y n) cost
+-- fqmem :: MonadReader Cost m => Int -> Int -> Int -> m [Double]
+-- fqmem = memo3 fq
 
--- | memoized version of fv
---
--- prop> \(NonNegative n) (Bounded x) (Bounded y) -> runReader (fvmem n x y) cost == runReader (fv n x y) cost
-fvmem :: Int -> Int -> Int -> Reader Cost Double
-fvmem = memo3 fv
+-- -- | memoized version of fv
+-- --
+-- -- prop> \(NonNegative n) (Bounded x) (Bounded y) -> runReader (fvmem n x y) cost == runReader (fv n x y) cost
+-- fvmem :: MonadReader Cost m => Int -> Int -> Int -> m Double
+-- fvmem = memo3 fv
 
 -- | calculates Q 
 --
@@ -88,14 +101,15 @@ fvmem = memo3 fv
 --
 -- This property says V always <= min Q. Not holds for n == 0
 -- prop> \(Positive n) (Bounded x) (Bounded y) -> (minimum $ runReader (fq x y n) cost) >= runReader (fv n x y) cost
-fq  :: Int          -- ^ x coordinate 
-    -> Int          -- ^ y coordinate
-    -> Int          -- ^ step number
-    -> Reader Cost [Double]
+fq  :: Dynamic n r m --(Integral n, Real r, MonadReader Cost m, MonadMemo (n, n, n) [r] m)
+    => n          -- ^ x coordinate 
+    -> n          -- ^ y coordinate
+    -> n          -- ^ step number
+    -> m [r]
 fq _ _ 0 = return [0, 0, 0, 0]           -- Q at 0 step is 0 in all directions
 fq x y n = do
     c <- ask
-    let v = fvmem n
+    let v = fv n
         q = fmap (c (x, y) +) . uncurry v
         pts = neighbours (x, y)
     traverse q pts
@@ -110,29 +124,30 @@ fq x y n = do
 --
 -- V is symmetric to changing sign of x and/or y
 -- prop> \(NonNegative n) (Bounded x) (Bounded y) -> runReader (fv n x y) cost == runReader (fv n (-x) (-y)) cost
-fv  :: Int      -- ^ step number 
-    -> Int      -- ^ x coordinate 
-    -> Int      -- ^ y coordinate
-    -> Reader Cost Double
+fv  :: Dynamic n r m --(Integral n, Real r, MonadReader Cost m, MonadMemo (n, n, n) [r] m)
+    => n --Int      -- ^ step number 
+    -> n --Int      -- ^ x coordinate 
+    -> n --Int      -- ^ y coordinate
+    -> m r
 fv _ 0 0 = do
     c <- ask
     return $ c (0, 0)               -- V at (0, 0) is 0 at any atep
 fv 0 x y = do
-    c<- ask
+    c <- ask
     return $ c (x, y)               -- V at 0 step is a cost
 fv n x y | y' > x' = fv n y' x'     -- function is symmetric to change x by y (due to properties of cost function)
          | x' > 100 = return 1000000000    -- limits on the board size
-         | otherwise = liftM minimum $ fqmem x' y' (n - 1)
+         | otherwise = liftM minimum $ for3 memo fq x' y' (n - 1)
     where x' = abs x
           y' = abs y
           
 -- | cost function
 --
-cost :: Pos -> Double
+cost :: (Integral n, Floating r) => (n, n) -> r
 cost (x, y) = 1 / (1 + exp (-s)) -- if s < 5 then s else 5
     where s = fromIntegral $ abs x + abs y
 
 -- | gives 4 neighbour points
 --
-neighbours :: Pos -> [Pos]
+neighbours :: Integral n => (n, n) -> [(n, n)]
 neighbours (x, y) = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
--- src/Markov.hs
+++ src/Markov.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE  FlexibleContexts
+            , ConstraintKinds #-}
+
 module Markov where
 
 import Control.Monad.Reader
@@ -10,7 +13,7 @@ import Dynamic
 -- >>> import Control.Arrow
 --
 -- >>> newtype Bounded = Bounded Int deriving Show
--- >>> instance Arbitrary Bounded where arbitrary = Bounded . (`mod` 4) <$> arbitrary
+-- >>> instance Arbitrary Bounded where arbitrary = Bounded . (`mod` 3) <$> arbitrary
 -- >>> newtype Command = Command Int deriving Show
 -- >>> instance Arbitrary Command where arbitrary = Command . abs . (`mod` 4) <$> arbitrary
 --
@@ -29,10 +32,11 @@ import Dynamic
 -- --->>> uncurry (<) $ (!! 1) &&& (!! 3) $ runReader (markovOut (0,0) [(1,0), (0,3)] [0.5, 0.5]) cost
 -- ---True
 -- this is not ture actually
-markovOut   :: Pos      -- ^ initial position
-            -> [Pos]    -- ^ list of targets positions
-            -> [Double] -- ^ list of probabilities (should sum to 1) for each target (lengths should be the same)
-            -> Reader Cost [Double] -- ^ 4 cost values for each of 4 directions
+markovOut   :: Dynamic n r m
+            => (n, n)      -- ^ initial position
+            -> [(n, n)]    -- ^ list of targets positions
+            -> [r] -- ^ list of probabilities (should sum to 1) for each target (lengths should be the same)
+            -> m [r] -- ^ 4 cost values for each of 4 directions
 markovOut x0 xs pr = do
     costs <- traverse (liftM fst . dynamic x0) xs
     let f p cs = (p *) <$> cs
@@ -46,13 +50,14 @@ markovOut x0 xs pr = do
 --
 -- Probability is less than 1
 -- prop> \(p0::BoundedPos) (p1::BoundedPos) -> runReader (bayesPrior 3 (b2p p0) (b2p p1)) cost <= 1.0
-bayesPrior  :: Int      -- ^ command
-            -> Pos      -- ^ current position
-            -> Pos      -- ^ target position
-            -> Reader Cost Double   -- ^ apriory probability of user choosing command in current situation
+bayesPrior  :: Dynamic n r m
+            => n      -- ^ command
+            -> (n, n)      -- ^ current position
+            -> (n, n)      -- ^ target position
+            -> m r   -- ^ apriory probability of user choosing command in current situation
 bayesPrior a p0 p = do
     (q, v) <- dynamic p0 p
-    return $ exp $ v - (q !! a)
+    return $ exp $ v - (q !! fromIntegral a)
 
 
 -- | 'bayesPosterior' posteriory probability of particular target given command a
@@ -60,15 +65,15 @@ bayesPrior a p0 p = do
 --
 -- Probability is less than 1
 -- prop> \(Command a) (p::BoundedPos) (NonEmpty (ps::[BoundedPos])) (Positive n) -> runReader (bayesPosterior a (b2p p) (b2p <$> ps) (n `mod` length ps)) cost <= 1.0 
-bayesPosterior  :: Int      -- ^ command
-                -> Pos      -- ^ current position
-                -> [Pos]    -- ^ target positions
-                -> Int      -- ^ target index
-                -> Reader Cost Double   -- ^ posteriory probability after user issued some command
+bayesPosterior  :: Dynamic n r m
+                => n      -- ^ command
+                -> (n, n)      -- ^ current position
+                -> [(n, n)]    -- ^ target positions
+                -> n      -- ^ target index
+                -> m r   -- ^ posteriory probability after user issued some command
 bayesPosterior a p0 ps n = do
     let prob = bayesPrior a p0
-    
-    p <- prob (ps !! n) 
+    p <- prob (ps !! fromIntegral n) 
     ps <- liftM sum (traverse prob ps)
     return $ p / ps
 
@@ -78,16 +83,17 @@ bayesPosterior a p0 ps n = do
 -- prop> \(Bounded x) (Bounded y) (Bounded x1) (Bounded x2) (Bounded y1) (Bounded y2) -> runReader (markovInEach (x, y) [(x1,y1),(x2,y2)] [0.75,0.25] 0 0) cost <= 1.0
 -- prop \(p::BoundedPos) (NonEmpty (ps::[BoundedPos])) (NonEmpty (pr::[Positive Int])) (Positive n) (Command a) -> 
 --          markovInEach (b2p p) (b2p <$> ps) (((/ (fromIntegral.sum) (getPositive<$>pr)).fromIntegral.getPositive) <$> pr) (n `mod` length ps) a
-markovInEach    :: Pos      -- ^ current position
-                -> [Pos]    -- ^ positons of targets
-                -> [Double] -- ^ probabilities of targets before user command
-                -> Int      -- ^ index of target
-                -> Int      -- ^ command issued by user
-                -> Reader Cost Double   -- ^ new probability of target
+markovInEach    :: Dynamic n r m
+                => (n, n)      -- ^ current position
+                -> [(n, n)]    -- ^ positons of targets
+                -> [r] -- ^ probabilities of targets before user command
+                -> n      -- ^ index of target
+                -> n      -- ^ command issued by user
+                -> m r   -- ^ new probability of target
 markovInEach p ps pr n a =  do
     let bayes = bayesPosterior a p ps
-    bayeses <- sequence $ bayes <$> [0 .. length pr - 1]
-    p <- liftM (* (pr !! n)) (bayes n) 
+    bayeses <- sequence $ bayes <$> [0 .. fromIntegral (length pr - 1)]
+    p <- liftM (* (pr !! fromIntegral n)) (bayes n) 
     let ps = (sum . zipWith (*) pr) bayeses
     return $ p / ps
 
@@ -95,12 +101,13 @@ markovInEach p ps pr n a =  do
 -- 
 -- All probabilities should sum to 1
 -- prop> \(Bounded x) (Bounded y) (Bounded x1) (Bounded x2) (Bounded y1) (Bounded y2) -> abs (sum (runReader (markovIn (x, y) [(x1,y1),(x2,y2)] [0.75,0.25] 1) cost)- 1.0) < 1e-2
-markovIn    :: Pos      -- ^ current position
-            -> [Pos]    -- ^ positons of targets
-            -> [Double] -- ^ probabilities of targets before user command
-            -> Int      -- ^ command issued by user
-            -> Reader Cost [Double] -- ^ new probability of target
+markovIn    :: Dynamic n r m
+            => (n, n)      -- ^ current position
+            -> [(n, n)]    -- ^ positons of targets
+            -> [r] -- ^ probabilities of targets before user command
+            -> n      -- ^ command issued by user
+            -> m [r] -- ^ new probability of target
 markovIn p ps pr a = do
     let markovInEach' n = markovInEach p ps pr n a
-    sequence $ markovInEach' <$> [0 .. length pr - 1]
+    sequence $ markovInEach' <$> [0 .. fromIntegral (length pr - 1)]
     
